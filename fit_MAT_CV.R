eda4b3e283462067336c7d317f3085f5d86e2849
jtipton25 <jtipton25@users.noreply.github.com>
2018-02-22 11:41:47 -0700

    Update fit_MAT_CV.R
    Updated documentation

--- fit_MAT_CV.R
+++ fit_MAT_CV.R
@@ -1,4 +1,18 @@
-fit_MAT_CV <- function(y_train_prop, X_train, k=k, sse=TRUE, nboot=1000, lean=FALSE, ...) {
+# fits modern-analog model on cross-validation data
+# y_train_prop A data frame of relative abundances of size N_site by N_taxa 
+# X_train A data frame of the climate covariate of size N_site by 1
+# k is the number of analogs
+# sse Sum of Squared Error ...
+# n.boot Number of bootstrapped samples for prediction
+# lean verbose model output
+#
+# returns a list of cross-validation statistics for each held out sample
+# MSPE Squared Prediction Error
+# MAE Absolute Error
+# CRPS Continuous Ranked Probability Score
+# coverage Empirical 95% coverage rate
+
+fit_MAT_CV <- function(y_train_prop, X_train, k=k, sse=TRUE, n.boot=1000, lean=FALSE, ...) {
   ## Modern analogue technique
   modMAT <- rioja::MAT(as.data.frame(y_train_prop), X_train, k=k, lean=lean, ...)
   predMAT <- predict(modMAT, as.data.frame(y_test_prop), k=k, sse=sse, n.boot=n.boot, ...)
