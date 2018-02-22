214918700a67da35390b36271c24d977e487fea3
Andria Dawson <andria.dawson@gmail.com>
2018-02-22 13:55:10 -0700

    Adding counts2proportions.R function

---
+++ counts2proporptions.R
@@ -0,0 +1,12 @@
+# converts count data to proportions
+#
+# counts A data frame of pollen counts of size N_sites by N_taxa
+# returns a data frame of the same size 
+# Non-zero rows sum to 1
+# Zero rows remain 0
+
+counts2proportions <- function(counts){
+
+  return(t(apply(counts, 1, function(x) if (sum(x)>0) {x/sum(x)} else {0})))
+  
+}
